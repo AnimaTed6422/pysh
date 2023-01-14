d6a7ce1d4622c6b24dbdcdb0e542afb8fc9251f7
AnimaTed <chervonijpes@gmail.com>
2023-01-14 17:47:16 -0500

    Finish Installer

--- install.py
+++ install.py
@@ -1,5 +1,21 @@
-"""
-The install feature is not finished yet.
-"""
 import requests
+import base64
+import os
 print("Installing PySh...")
+
+os.mkdir("pysh")
+
+contentURL = "https://api.github.com/repos/AnimaTed6422/pysh/contents/"
+
+files = ["__init__.py", "external.py", "internal.py", "meta.json", "shell.py"]
+
+for file in files:
+    with open("pysh/" + file, 'w') as fh:
+        fh.write(base64.b64decode(requests.get(contentURL + file).json()["content"]).decode())
+    print("- {}".format(file))
+
+for folder in ["programs", "scripts"]:
+    os.mkdir("pysh/" + folder)
+    print("- {}".format(folder))
+
+print("Successfully installed PySh")
